ca12f69db4b530dadb5af1e84b64c5cd25c0614c
adiiigo <aditi.k.goyal@gmail.com>
2020-03-30 23:05:41 +0530

    Linked API service to the front

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose") ;
 
+MONGOURI="mongodb+srv://aditi:aditi123@cluster0-sacre.mongodb.net/test?retryWrites=true&w=majority"
+
 const IntiateMongoServer = async () => {
     try{
-
         await mongoose.connect(MONGOURI, {
            useNewUrlParser: true 
         });
         console.log("Connected to DB !!") ;
     }catch(e){
-        console.log(e);
+        console.log("Not Connected to Database ERROR! ", e);
         throw e ;
     }
 };
--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const express = require("express") ;
 const bodyparser = require("body-parser");
 const InitiateMongoServer = require("./config/db");
 
-InitiateMongoServer();
+// InitiateMongoServer();
 
 const app = express() ;
 
-const PORT = process.env.PORT || 4000 ;
+// const PORT = process.env.PORT || 4000 ;
+
+app.set("port", process.env.PORT || 4000);
+
+// app.use(express.static("client/build"));
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    console.log("API WORKING!!")
+  }
 
 app.use(bodyparser.json());
 
 app.get("/" , (req, res) => {
     res.json({message: "API Working"})
+
+    console.log("API Working") ;
 }) ;
 
-app.listen(PORT, (req, res) => {
-    console.log('Server Started at PORT `($PORT)`') ;
-}) ;
+// app.listen(PORT, (req, res) => {
+//     console.log('Server Started at PORT `($PORT)`') ;
+// }) ;
+
+app.listen(app.get("port"), () => {
+    console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+  });
 
